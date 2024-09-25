1d4c77ef0d5ac3cb64b910ae3bc1a88ad6da12dc
Ibrahim Ivan <hl2635@wayne.edu>
2024-09-25 05:50:18 -0400

    cool

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,8 @@
 
-import Header from "./Header.jsx"
-import Footer from "./Footer.jsx";
-import Food from "./Food.jsx";
-
 function App() {
   return(
     <>
-      <Header/>
-      <Food/>
-      <Food/>
-      <Food/>
-      <Footer/>
+      
     </>
   );
 }
